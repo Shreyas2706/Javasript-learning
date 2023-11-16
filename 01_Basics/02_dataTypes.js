ef1ee643f139064665b5303f86062f88baee8229
Shreyas Singh <128822637+Shreyas2706@users.noreply.github.com>
2023-11-16 11:51:08 +0000

    datatype done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" ; // treat all JS code as newer version 
+
+//alert("shevy") // we are using node js , not browser
+
+console.log(3 + 3
+    ) // code readibilty should be high 
+
+    console.log("Shevy")
+
+    let name = "shevy"
+    let age = 18 
+
+    let islooggeIn = true 
+
+    // number => 2 to the power 53 
+    // bigint 
+    // string => ""
+    // boolean => true / false 
+    // null => standalone value 
+    // undifiend =>
+
+    // symbol => unique 
+
+    // object 
+
+    console.log(typeof age) //   number 
+    console,log(typeof undefined) // undefined
+    console.log(typeof null) // object 
+    
