25647df8ce4babebe3e1bebd89e4704197110ca7
Shreyas Singh <128822637+Shreyas2706@users.noreply.github.com>
2023-11-18 07:10:14 +0000

    notes making

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,7 +1,10 @@
 "use strict" ;
-let score = "33" 
+let score = "33abc" 
 console.log(typeof (score))
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber) 
 console.log(valueInNumber)
+
+// "33" => 33 
+// "33abc" ==
